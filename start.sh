65f0ee4c177c3c521c6da3a07f26d2f09627f871
Hsk <149724465+Hskdashing@users.noreply.github.com>
2023-11-26 00:30:48 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hskdashing/DQhsk
+   git clone https://github.com/Hskdashing/DQhsk.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO https://github.com/Hskdashing/DQhsk
+  git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
